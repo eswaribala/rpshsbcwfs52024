2c0fe3255f9aa604d01f1ff53497de88614cd142
Parameswari Bala <parameswaribala@gmail.com>
2024-08-08 14:30:43 +0530

    updated

---
+++ pharmaui/scripts/index.js
@@ -0,0 +1,8 @@
+window.addEventListener('load',function(){
+
+    document.querySelector('#singUp').addEventListener('click',
+        function(){
+       // alert("open signup");
+       window.location.href="signup.html"
+    })
+});
