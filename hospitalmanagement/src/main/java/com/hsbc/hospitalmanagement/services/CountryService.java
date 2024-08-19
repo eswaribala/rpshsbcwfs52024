3d6bcbe8c40f192edf26a46c1b0e45a0568ea15d
Parameswari Bala <parameswaribala@gmail.com>
2024-08-19 11:44:21 +0530

    updated

--- hospitalmanagement/src/main/java/com/hsbc/hospitalmanagement/services/CountryService.java
+++ hospitalmanagement/src/main/java/com/hsbc/hospitalmanagement/services/CountryService.java
@@ -28,41 +28,15 @@ public class CountryService {
        ResponseEntity responseEntity =restTemplate.getForEntity(countryUrl,
                List.class);
        List<Object> countriesList= (List<Object>) responseEntity.getBody();
-       for(Object object : countriesList)
-           parseString(object.toString());
+       JSONObject jsonObject=null;
+       for(Object object : countriesList){
+           jsonObject=new JSONObject((Map)object);
+           System.out.println(jsonObject.get("name"));
+       }
         return null;
-
     }
 
-    private void parseString(String response)
-    {
-        JSONParser parser = new JSONParser();
-        String name="";
-        try {
 
-            // Put above JSON content to crunchify.txt file and change path location
-            Object obj = parser.parse(response);
-            JSONObject jsonObject = (JSONObject) obj;
-
-            // JsonFlattener: A Java utility used to FLATTEN nested JSON objects
-            String flattenedJson = JsonFlattener.flatten(jsonObject.toString());
-            //log("\n=====Simple Flatten===== \n" + flattenedJson);
-
-            Map<String, Object> flattenedJsonMap = JsonFlattener.flattenAsMap(jsonObject.toString());
-            name=(String) flattenedJsonMap.get("name");
-            //log("\n=====Flatten As Map=====\n" + flattenedJson);
-            // We are using Java8 forEach loop. More info: https://crunchify.com/?p=8047
-            //flattenedJsonMap.forEach((k, v) -> log(k + " : " + v));
-
-            // Unflatten it back to original JSON
-            String nestedJson = JsonUnflattener.unflatten(flattenedJson);
-            System.out.println("\n=====Unflatten it back to original JSON===== \n" + nestedJson);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-       // return token;
-
-    }
 
 
 }
