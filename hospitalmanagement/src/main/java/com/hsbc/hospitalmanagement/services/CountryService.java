4545b14ec503ebfc8d0b9561af77831a3b53a5ec
Parameswari Bala <parameswaribala@gmail.com>
2024-08-19 11:24:50 +0530

    updated

--- hospitalmanagement/src/main/java/com/hsbc/hospitalmanagement/services/CountryService.java
+++ hospitalmanagement/src/main/java/com/hsbc/hospitalmanagement/services/CountryService.java
@@ -12,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -25,9 +26,10 @@ public class CountryService {
     public List<String> getCountryNames() throws NoSuchFieldException, IllegalAccessException {
 
        ResponseEntity responseEntity =restTemplate.getForEntity(countryUrl,
-               String.class);
-       parseString(responseEntity.getBody().toString());
-
+               List.class);
+       List<Object> countriesList= (List<Object>) responseEntity.getBody();
+       for(Object object : countriesList)
+           parseString(object.toString());
         return null;
 
     }
