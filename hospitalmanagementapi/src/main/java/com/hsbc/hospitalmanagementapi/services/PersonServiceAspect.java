3816b237b27482e84f8221f538e3fab7b4160757
Parameswari Bala <parameswaribala@gmail.com>
2024-08-21 12:20:14 +0530

    updated

--- hospitalmanagementapi/src/main/java/com/hsbc/hospitalmanagementapi/services/PersonServiceAspect.java
+++ hospitalmanagementapi/src/main/java/com/hsbc/hospitalmanagementapi/services/PersonServiceAspect.java
@@ -6,11 +6,15 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+@Component
 @Aspect
 @Slf4j
 public class PersonServiceAspect {
-    @Before(value = "execution(* com.hsbc.hospitalmanagementapi.services.PersonService..*(..)) " +
+    @Before(value = "execution(* com.hsbc.hospitalmanagementapi.services.*.add*(*))" +
             "&& args(person)")
     public void monitorBeforeAddPerson(JoinPoint joinPoint, Person person){
 
@@ -25,4 +29,13 @@ public class PersonServiceAspect {
         log.info("Exiting Add Person method......");
         log.info("Add Person method created new object...."+person);
     }
+
+
+    @AfterReturning(pointcut="execution(* getAllPersons())", returning="returnData")
+    public void getNameReturningAdvice(List<Person> returnData){
+       for (Person person : returnData)
+           log.info("Data Received as"+person);
+    }
+
+
 }
