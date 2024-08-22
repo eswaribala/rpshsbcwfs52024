7c50cad59edaa558b045e18697d2c5564aea0595
Parameswari Bala <parameswaribala@gmail.com>
2024-08-22 12:39:30 +0530

    updated

--- hsbcapicloudgateway/src/main/java/com/hsbc/hsbcapicloudgateway/HsbcapicloudgatewayApplication.java
+++ hsbcapicloudgateway/src/main/java/com/hsbc/hsbcapicloudgateway/HsbcapicloudgatewayApplication.java
@@ -2,8 +2,10 @@ package com.hsbc.hsbcapicloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HsbcapicloudgatewayApplication {
 
     public static void main(String[] args) {
