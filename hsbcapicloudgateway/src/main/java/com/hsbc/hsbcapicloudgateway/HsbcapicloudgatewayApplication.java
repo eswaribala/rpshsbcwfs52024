98f4ba7b24431554e4d23fb205a2d3f0bc673ba8
Parameswari Bala <parameswaribala@gmail.com>
2024-08-22 11:50:51 +0530

    updated

---
+++ hsbcapicloudgateway/src/main/java/com/hsbc/hsbcapicloudgateway/HsbcapicloudgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hsbc.hsbcapicloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsbcapicloudgatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsbcapicloudgatewayApplication.class, args);
+    }
+
+}
---
+++ hsbcapicloudgateway/src/test/java/com/hsbc/hsbcapicloudgateway/HsbcapicloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsbc.hsbcapicloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsbcapicloudgatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
