3043c1e2813d7356c3c32c3b62f000f88bef1586
Parameswari Bala <parameswaribala@gmail.com>
2024-08-23 16:17:23 +0530

    updated

--- uitraining/netflixapp/src/App.js
+++ uitraining/netflixapp/src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Logo from "./components/Logo/Logo";
+import {TimerComponent} from "./components/Timer/Timer";
 
 function App() {
   return (
       <div className="App">
           <header className="App-header">
+            {/* stateless component*/}
               <Logo/>
               <h1 className="multicolortext">Netflix</h1>
-
+              {/* stateful component*/}
+              <TimerComponent/>
           </header>
 
       </div>
--- uitraining/netflixapp/src/components/Timer/Timer.js
+++ uitraining/netflixapp/src/components/Timer/Timer.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {Component} from 'react';
 import styles from './Timer.module.css';
 
-const Timer = () => (
-  <div className={styles.Timer}>
-    Timer Component
-  </div>
-);
+export class TimerComponent extends Component{
+    constructor(props, context) {
+        super(props, context);
+    //step1
+        this.state={
+            currentTime:new Date()
+        }
+    }
+    //step2
+    timerEvent=()=>{
+        this.setState({
+            currentTime:new Date()
+        })
+    }
 
+  //step4
+    componentDidMount() {
+        setInterval(this.timerEvent,1000);
+    }
 
-export default Timer;
+    render() {
+        return(
+            <div>
+                <h4 >{this.state.currentTime.toLocaleTimeString()}</h4>
+            </div>
+        )
+    }
+}
