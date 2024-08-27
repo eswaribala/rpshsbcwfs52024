42b21cc09e96b8500b33825f5c166c38ba50282d
Parameswari Bala <parameswaribala@gmail.com>
2024-08-27 10:00:19 +0530

    updated

--- uitraining/netflixapp/src/components/HomeMenu/HomeMenu.js
+++ uitraining/netflixapp/src/components/HomeMenu/HomeMenu.js
@@ -1,27 +1,29 @@
 import React, {useState} from 'react';
 import styles from './HomeMenu.module.css';
 import {Tab, Tabs} from "@mui/material";
-
+import TabPanel from '@mui/lab/TabPanel';
+import {TabContext, TabList} from "@mui/lab";
 const HomeMenu = () => {
 
-    const [value,setValue]=useState('');
-    function handleChange(value){
+    const [value,setValue]=useState('1');
+    function handleChange(event,value){
         setValue(value);
     }
 
     return(
         <div className={styles.HomeMenu}>
-            <Tabs
-                value={value}
-                onChange={handleChange}
-                textColor="secondary"
-                indicatorColor="secondary"
-                aria-label="secondary tabs example"
-            >
-                <Tab value="one" label="Item One" />
-                <Tab value="two" label="Item Two" />
-                <Tab value="three" label="Item Three" />
-            </Tabs>
+            <TabContext value={value}>
+
+                    <TabList onChange={handleChange} aria-label="lab API tabs example">
+                        <Tab label="Item One" value="1" />
+                        <Tab label="Item Two" value="2" />
+                        <Tab label="Item Three" value="3" />
+                    </TabList>
+            
+                <TabPanel value="1">Item One</TabPanel>
+                <TabPanel value="2">Item Two</TabPanel>
+                <TabPanel value="3">Item Three</TabPanel>
+            </TabContext>
         </div>
     )
 };
