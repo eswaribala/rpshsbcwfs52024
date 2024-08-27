9ab63d12e2c959f6f6c2950628ff77d8fa4cb40a
Parameswari Bala <parameswaribala@gmail.com>
2024-08-27 10:02:04 +0530

    updated

--- uitraining/netflixapp/src/components/HomeMenu/HomeMenu.js
+++ uitraining/netflixapp/src/components/HomeMenu/HomeMenu.js
@@ -19,7 +19,7 @@ const HomeMenu = () => {
                         <Tab label="Item Two" value="2" />
                         <Tab label="Item Three" value="3" />
                     </TabList>
-            
+
                 <TabPanel value="1">Item One</TabPanel>
                 <TabPanel value="2">Item Two</TabPanel>
                 <TabPanel value="3">Item Three</TabPanel>
